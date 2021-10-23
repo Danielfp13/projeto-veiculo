93930c2659b16374516173d5be4695480c650113
Daniel <danielfernando-2@outlook.com>
2021-10-23 10:21:20 -0300

    Mapeamento objeto relacional

---
+++ Back-end/src/main/java/com/api/veiculo/entities/Veiculo.java
@@ -0,0 +1,40 @@
+package com.api.veiculo.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Veiculo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String veiculo;
+	private String marca;
+	private Long ano;
+	private String descricao;
+	private Boolean vendido;
+	
+	@JsonFormat(pattern = "dd-MM-yyyy")
+	private LocalDateTime created;
+	
+	@JsonFormat(pattern = "dd-MM-yyyy")
+	private LocalDateTime updated;
+}
