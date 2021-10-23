5f2b9c9572436f22678a6948a3835aeb59452b4a
Daniel <danielfernando-2@outlook.com>
2021-10-23 14:45:47 -0300

    Criando endpoint cadastrar veiculo

--- Back-end/src/main/java/com/api/veiculo/controller/VeiculoController.java
+++ Back-end/src/main/java/com/api/veiculo/controller/VeiculoController.java
@@ -1,14 +1,21 @@
 package com.api.veiculo.controller;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.api.veiculo.dto.VeiculoDTO;
 import com.api.veiculo.entity.Veiculo;
 import com.api.veiculo.service.VeiculoService;
 
@@ -33,4 +40,12 @@ public class VeiculoController {
 		return ResponseEntity.ok().body(veiculo);
 	}
 
+	// Adiciona um veiculo
+	@PostMapping
+	public ResponseEntity<Veiculo> insert(@Valid @RequestBody VeiculoDTO veiculoDTO) {
+		Veiculo veiculo = veiculoService.insert(veiculoDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(veiculo.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(veiculo);
+	}
 }
--- Back-end/src/main/java/com/api/veiculo/service/VeiculoService.java
+++ Back-end/src/main/java/com/api/veiculo/service/VeiculoService.java
@@ -1,10 +1,13 @@
 package com.api.veiculo.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.veiculo.dto.VeiculoDTO;
 import com.api.veiculo.entity.Veiculo;
 import com.api.veiculo.repository.VeiculoRepository;
 import com.api.veiculo.service.exception.ObjectNotFoundException;
@@ -25,5 +28,13 @@ public class VeiculoService {
 	public Veiculo find(Long id) {
 		return veiculoRepository.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundException("Não exite veiculo com id = " + id + "."));
+	}
+	
+	// inserir veiculo
+	public Veiculo insert(VeiculoDTO veiculoDTO) {
+		Veiculo veiculo = new Veiculo();
+		BeanUtils.copyProperties(veiculoDTO, veiculo);
+		veiculo.setCreated(LocalDateTime.now());
+		return veiculoRepository.save(veiculo);
 	}
 }
